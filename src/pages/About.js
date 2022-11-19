827681fd6f6a31da60f70f70a1467b50b804d6a7
kkli2e <kkli2e@pantarei-design.com>
2022-11-20 00:42:18 +0800

    add breadcrumb

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink, Link} from "react-router-dom";
+import React from "react";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { Container } from "../GlobalStyle";
+import "../index.scss";
+
 function About() {
+    const breadcrumbs = useBreadcrumbs();
+    console.log(breadcrumbs.length);
+    let activeStyle = {
+        color: "#1cb7ef"
+    };
     return (
-        <Outlet />
+        <>
+            <Container>
+                <nav className="breadcrumb">
+                    {breadcrumbs.map(({ match, breadcrumb }, index) => (
+                        <React.Fragment key={match.pathname}> 
+                            <NavLink
+                                to={match.pathname}
+                                // style={({ isActive }) =>
+                                //     isActive ? activeStyle : undefined
+                                // }
+                            >
+                                {breadcrumb}
+                            </NavLink>
+                            {index === breadcrumbs.length-1 ? null : (<span>/</span>)}
+                        </React.Fragment>
+                    ))}
+                </nav>
+            </Container>
+            <Outlet />
+        </>
     );
 }
 
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -7,122 +7,146 @@ function Pricing() {
                 <h1>Pricing</h1>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Initial Physiotherapy Rates</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>Standard Initial Appointment (30 Mins)</td>
-                            <td>$67</td>
-                        </tr>
-                        <tr>
-                            <td>Extended Initial Appointment (60 Mins)</td>
-                            <td>$134</td>
-                        </tr>
-                        <tr>
-                            <td>Senior Physiotherapist Initial Appointment (30 Mins)</td>
-                            <td>$105</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Initial Physiotherapy Rates</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Standard Initial Appointment (30 Mins)</td>
+                                <td>$67</td>
+                            </tr>
+                            <tr>
+                                <td>Extended Initial Appointment (60 Mins)</td>
+                                <td>$134</td>
+                            </tr>
+                            <tr>
+                                <td>Senior Physiotherapist Initial Appointment (30 Mins)</td>
+                                <td>$105</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Follow-Up Physiotherapy Rates</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>Short Follow-Up Appointment (20mins)</td>
-                            <td>$80</td>
-                        </tr>
-                        <tr>
-                            <td>Standard Follow-Up Appointment (30 Mins)</td>
-                            <td>$95</td>
-                        </tr>
-                        <tr>
-                            <td>Extended Follow-Up Appointment (60 Mins)</td>
-                            <td>$180</td>
-                        </tr>
-                        <tr>
-                            <td>Senior Physiotherapist Standard Follow-Up Appointment (30 Mins)</td>
-                            <td>$95</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Follow-Up Physiotherapy Rates</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Short Follow-Up Appointment (20mins)</td>
+                                <td>$80</td>
+                            </tr>
+                            <tr>
+                                <td>Standard Follow-Up Appointment (30 Mins)</td>
+                                <td>$95</td>
+                            </tr>
+                            <tr>
+                                <td>Extended Follow-Up Appointment (60 Mins)</td>
+                                <td>$180</td>
+                            </tr>
+                            <tr>
+                                <td>Senior Physiotherapist Standard Follow-Up Appointment (30 Mins)</td>
+                                <td>$95</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Medicare & Department of Veterans Affairs Claims</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>EPC/Medicare Care Plans (30mins)</td>
-                            <td>$36</td>
-                        </tr>
-                        <tr>
-                            <td>Department of Veterans Affairs (30 Mins)</td>
-                            <td>Bulk Billing</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Medicare & Department of Veterans Affairs Claims</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>EPC/Medicare Care Plans (30mins)</td>
+                                <td>$36</td>
+                            </tr>
+                            <tr>
+                                <td>Department of Veterans Affairs (30 Mins)</td>
+                                <td>Bulk Billing</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Physiotherapist Exercise and Rehab Sessions: (Gym Only)</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>Standard Rehab Consult (30mins)</td>
-                            <td>$90</td>
-                        </tr>
-                        <tr>
-                            <td>Block of 10 Rehab Consults (30mins)</td>
-                            <td>$80 ($800)</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Physiotherapist Exercise and Rehab Sessions: (Gym Only)</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Standard Rehab Consult (30mins)</td>
+                                <td>$90</td>
+                            </tr>
+                            <tr>
+                                <td>Block of 10 Rehab Consults (30mins)</td>
+                                <td>$80 ($800)</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Remedial Massage Rates (Private Health Benefits Apply)</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>Initial Remedial Massage Appointment (45mins)</td>
-                            <td>$75</td>
-                        </tr>
-                        <tr>
-                            <td>30 Minute Remedial Massage (30mins)</td>
-                            <td>$80</td>
-                        </tr>
-                        <tr>
-                            <td>45 Minute Remedial Massage (45mins)</td>
-                            <td>$95</td>
-                        </tr>
-                        <tr>
-                            <td>60 Minute Remedial Massage (60mins)</td>
-                            <td>$120</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Remedial Massage Rates (Private Health Benefits Apply)</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Initial Remedial Massage Appointment (45mins)</td>
+                                <td>$75</td>
+                            </tr>
+                            <tr>
+                                <td>30 Minute Remedial Massage (30mins)</td>
+                                <td>$80</td>
+                            </tr>
+                            <tr>
+                                <td>45 Minute Remedial Massage (45mins)</td>
+                                <td>$95</td>
+                            </tr>
+                            <tr>
+                                <td>60 Minute Remedial Massage (60mins)</td>
+                                <td>$120</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 <div className="table_wrapper">
                     <table>
-                        <tr>
-                            <th>Products</th>
-                            <th>Price</th>
-                        </tr>
-                        <tr>
-                            <td>Spikey Ball/ Trigger Point Ball</td>
-                            <td>$15</td>
-                        </tr>
-                        <tr>
-                            <td>Short Foam Roller</td>
-                            <td>$30</td>
-                        </tr>
-                        <tr>
-                            <td>Long Foam Roller</td>
-                            <td>$65</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>Products</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Spikey Ball/ Trigger Point Ball</td>
+                                <td>$15</td>
+                            </tr>
+                            <tr>
+                                <td>Short Foam Roller</td>
+                                <td>$30</td>
+                            </tr>
+                            <tr>
+                                <td>Long Foam Roller</td>
+                                <td>$65</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </Container>
